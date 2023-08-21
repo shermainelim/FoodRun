84d5d3ff717e9d3fcbd128859f3eee0332f69b1b
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-08-21 22:39:12 +0800

    Feature: Welcome Page of Food Dash
    - added icons, background color and custom button component

--- client/src/screens/welcome/Welcome.tsx
+++ client/src/screens/welcome/Welcome.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import classNames from "classnames/bind";
 import styles from "./Welcome.scss";
-import Couple from "../../assets/couple5.png";
+import Eco from "../../assets/f1.png";
 import CustomButton from "../../shared/CustomButton";
 import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
@@ -24,12 +24,12 @@ const Welcome = () => {
           >
             <h1>Terms and Condition</h1>
             <p>
-              Couple Goals is committed to protecting the security of personal data. <br /> <br />
+              Food Dash is committed to protecting the security of personal data. <br /> <br />
               <br /> While no security measure can guarantee against compromise, loss or misuse,<br /> <br />
               we use a variety of security technologies to protect data from unauthorized <br /> <br />
               access, use, or disclosure. 
               <br /> <br /> <br /> Personal data that you provide via the Application will not be shared<br /> <br />
-               outside of Couple Goals and its affiliates without your consent.<br /> <br />
+               outside of Food Dash and its affiliates without your consent.<br /> <br />
               
             </p>
 
@@ -55,28 +55,30 @@ const Welcome = () => {
       <img
         data-testid="img-logo-resident"
         className={cx("imageIcon")}
-        src={Couple}
+        src={Eco}
         alt="Logo"
       />
-      <div className={cx("title")}>Couple Goals</div>
+      <div className={cx("title")}>Food Dash</div>
 
       <CustomButton
         className="resident-btn"
         testId="resident"
-        content="Create a Couple Space"
+        content="Login"
+        clicked={() => {
+          navigate("/loginOptions");
+        }}
+      ></CustomButton>
+
+      <CustomButton
+        className="resident-btn"
+        testId="resident"
+        content="Register"
         clicked={() => {
           navigate("/register");
         }}
       ></CustomButton>
 
-      <CustomButton
-        className="resident-btn"
-        testId="resident"
-        content="Login to Couple Space"
-        clicked={() => {
-          navigate("/loginOptions");
-        }}
-      ></CustomButton>
+     
 
       <CustomButton
         className="resident-btn"
